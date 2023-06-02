ff45de425f230b14c4e8ab871bea8acb77740f54
kirill shubrat <kirill.shubrat@mail.ru>
2023-06-02 11:35:54 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SERVER_URL : process.env.SERVER_URL,
+    APP_URL: process.env.APP_URL
+  },
+  images:{domains:['loremflickr.com', 'www.aptronixindia.com','cloudflare-ipfs.com','live.staticflickr.com', 'i.pinimg.com']}
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import AuthProvider from '@/providers/auth-provider/AuthProvider'
+import { TypeComponentAuthFields } from '@/providers/auth-provider/auth-page.types'
+
+import '@/assets/styles/globals.scss'
+
+import { persistor, store } from '@/store/store'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
+
+export default function App({
+	Component,
+	pageProps
+}: AppProps & TypeComponentAuthFields) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<AuthProvider Component={{ isOnlyUser: Component.isOnlyUser }}>
+						<Component {...pageProps} />
+					</AuthProvider>
+				</PersistGate>
+			</Provider>
+		</QueryClientProvider>
+	)
 }
